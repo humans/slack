09ca72c7f1c9b8e5c545dcf57767b30e5f0d06b1
Jaggy Gauran <i.am@jag.gy>
2017-04-07 19:18:14 +0800

    Bind axios to the window so echo can bind the socket id.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -3,13 +3,14 @@ import Vue from 'vue';
 import Holler from './holler';
 
 axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.Slack.csrfToken,
-    'X-Requested-With': 'XMLHttpRequest',
+  'X-CSRF-TOKEN': window.Slack.csrfToken,
+  'X-Requested-With': 'XMLHttpRequest',
 };
 
-Vue.prototype.$http = axios;
+Vue.prototype.$http = window.axios = axios;
 Vue.prototype.$echo = window.echo = new Holler({
-    broadcaster: 'pusher',
-    key: 'aedd54192305c7a81468',
-    cluster: 'ap1',
+  broadcaster: 'pusher',
+  key: 'aedd54192305c7a81468',
+  cluster: 'ap1',
 });
+
