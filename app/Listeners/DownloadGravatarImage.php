b36fdb14e10deaf98121c71b4b5a90d5b6aa092e
Jaggy Gauran <i.am@jag.gy>
2017-04-09 14:57:22 +0800

    Set up the events and the database queue.

---
+++ app/Listeners/DownloadGravatarImage.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class DownloadGravatarImage
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserRegistered  $event
+     * @return void
+     */
+    public function handle(UserRegistered $event)
+    {
+        // $event->user->image = 
+        //
+    }
+}
